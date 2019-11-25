91ed2aae341d49ca5d0a624a31f10f31f1debd0e
vanya <vanya.moo@gmail.com>
2019-11-25 21:56:38 +0000

    Convert to MVC design pattern

---
+++ Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Vanya Mutafchieva on 25/11/2019.
+//  Copyright © 2019 Vanya Mutafchieva. All rights reserved.
+//
+
+import Foundation
+
+struct Item {
+    var title: String = ""
+    var done: Bool = false
+}
