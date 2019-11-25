ad68cfbfe03540c7bd6f2990cf902da9fb641ea5
vanya <vanya.moo@gmail.com>
2019-11-25 14:36:22 +0000

    Initial commit

---
+++ TodoListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Vanya Mutafchieva on 25/11/2019.
+//  Copyright © 2019 Vanya Mutafchieva. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
